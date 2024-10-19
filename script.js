a06607d8f8d5db13def50f19dabdaf192d3ab535
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-18 21:42:32 -0500

    modify index.html and add seperate scripg.js and styles.css files

---
+++ script.js
@@ -0,0 +1,6 @@
+// Adding interactivity to "Read More" buttons
+document.querySelectorAll('.read-more').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("More content will be added soon. Stay tuned!");
+    });
+});
